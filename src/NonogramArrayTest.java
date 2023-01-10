0a1928c2684a8a41dc7b2d884d84942b08b2e45a
Maciej Lipski <maciej.lipski2.stud@pw.edu.pl>
2023-01-10 13:24:59 +0100

    Zmiana numeracji drukowanych kolumn i rzędów

--- src/NonogramArrayTest.java
+++ src/NonogramArrayTest.java
@@ -6,7 +6,7 @@ public class NonogramArrayTest {
         // Print the row data
         System.out.println("Row data:");
         for (int i = 0; i < columns; i++) {
-            System.out.print("Row " + i + ": ");
+            System.out.print("Row " + (i+1) + ": ");
             for (int j = 0; j < data[i].size(); j++) {
                 System.out.print(data[i].get(j) + " ");
             }
@@ -16,7 +16,7 @@ public class NonogramArrayTest {
         // Print the column data
         System.out.println("\nColumn data:");
         for (int i = columns; i < data.length; i++) {
-            System.out.print("Column " + (i - columns) + ": ");
+            System.out.print("Column " + (i+1 - columns) + ": ");
             for (int j = 0; j < data[i].size(); j++) {
                 System.out.print(data[i].get(j) + " ");
             }
