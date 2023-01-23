d51b9dcc19101eff2ad024effcafdc2756449f0c
mtopolsk <michal.topolski.stud2@pw.edu.pl>
2023-01-23 13:39:02 +0100

    Style do eksport xlsx, stworzenie klasy Solver

--- src/main/java/NonogramExcelExporter.java
+++ src/main/java/NonogramExcelExporter.java
@@ -1,7 +1,4 @@
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
@@ -14,14 +11,16 @@ public class NonogramExcelExporter {
     private int columns;
     private String fileName;
     private String path;
+
     NonogramExcelExporter(ArrayList<Integer>[] nonogramArray, int columns,String fileName){
         this.nonogramArray = nonogramArray;
         this.columns = columns;
         this.fileName = fileName;
         Workbook workbook = new XSSFWorkbook();
         Sheet sheet = workbook.createSheet("Nonogram");
-        writeColumnData(this.nonogramArray,this.columns,sheet);
-        writeRowData(this.nonogramArray,this.columns,sheet);
+        CellStyle style = workbook.createCellStyle();
+        writeColumnData(this.nonogramArray,this.columns,sheet,style);
+        writeRowData(this.nonogramArray,this.columns,sheet,style);
         for (int i = 0; i <columns ; i++) {
             sheet.autoSizeColumn(i);
         }
@@ -41,15 +40,16 @@ public class NonogramExcelExporter {
             System.out.println("Error saving Excel file.");
         }
         String absolutePath = myFile.getAbsolutePath();
+
         return absolutePath;
     }
-    public void writeColumnData(ArrayList<Integer>[] nonogramArray, int columns,Sheet sheet) {
+    public void writeColumnData(ArrayList<Integer>[] nonogramArray, int columns,Sheet sheet,CellStyle style) {
         Row headerRow = sheet.createRow(0);
         Row dataRow = sheet.createRow(1);
         for (int i = columns; i < nonogramArray.length; i++) {
             Cell headerRowCell = headerRow.createCell(i+2-columns);
             Cell dataCell = dataRow.createCell(i+2-columns);
-            String columnHeader = "Column " + (i + 1 - columns);
+            String columnHeader = String.valueOf((i + 1 - columns));
             StringBuilder columnData = new StringBuilder();
             for (int j = 0; j < nonogramArray[i].size(); j++) {
                 columnData.append(nonogramArray[i].get(j) + ", ");
@@ -57,16 +57,30 @@ public class NonogramExcelExporter {
             if (columnData.length() > 0 && columnData.charAt(columnData.length() - 2) == ',') {
                 columnData.deleteCharAt(columnData.length() - 2);
             }
+
+            //Styling cells - black thin borders, counterclockwise rotation
+            style.setRotation((short) 45);
+            style.setBorderBottom(BorderStyle.THIN);
+            style.setBottomBorderColor(IndexedColors.BLACK.getIndex());
+            style.setBorderLeft(BorderStyle.THIN);
+            style.setLeftBorderColor(IndexedColors.BLACK.getIndex());
+            style.setBorderRight(BorderStyle.THIN);
+            style.setRightBorderColor(IndexedColors.BLACK.getIndex());
+            style.setBorderTop(BorderStyle.THIN);
+            style.setTopBorderColor(IndexedColors.BLACK.getIndex());
+            style.setFillBackgroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
+
             headerRowCell.setCellValue(columnHeader);
             dataCell.setCellValue(columnData.toString());
+            dataCell.setCellStyle(style);
         }
     }
-    public void writeRowData(ArrayList<Integer>[] nonogramArray, int columns,Sheet sheet){
+    public void writeRowData(ArrayList<Integer>[] nonogramArray, int columns,Sheet sheet, CellStyle style){
         for (int i = 0; i < columns; i++) {
             Row newRow = sheet.createRow(i+2);
             Cell headerCell= newRow.createCell(0);
             Cell dataCell= newRow.createCell(1);
-            String rowHeader= "Row " + (i + 1);
+            String rowHeader= String.valueOf((i + 1));
             StringBuilder rowData= new StringBuilder();
             for (int j = 0; j < nonogramArray[i].size(); j++) {
                 rowData.append(nonogramArray[i].get(j) + ", ");
@@ -74,8 +88,13 @@ public class NonogramExcelExporter {
             if (rowData.length() > 0 && rowData.charAt(rowData.length() - 2) == ',') {
                 rowData.deleteCharAt(rowData.length() - 2);
             }
+
+            //Styling cells - no rotation
+            //style.setRotation((short) 0);
+
             headerCell.setCellValue(rowHeader);
             dataCell.setCellValue(rowData.toString());
+            dataCell.setCellStyle(style);
         }
     }
 }
