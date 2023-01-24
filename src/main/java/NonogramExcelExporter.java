416c66a8b32584296d35ad03c0675512658f476e
ksrednic <01178306@pw.edu.pl>
2023-01-24 11:10:04 +0100

    Ostateczna wersja planszy w excelu, rotation 45

--- src/main/java/NonogramExcelExporter.java
+++ src/main/java/NonogramExcelExporter.java
@@ -62,7 +62,7 @@ public class NonogramExcelExporter {
             }
 
             //Styling cells - black thin borders, counterclockwise rotation
-            style.setRotation((short) 60);
+            style.setRotation((short) 45);
             style.setBorderBottom(BorderStyle.THIN);
             style.setBottomBorderColor(IndexedColors.BLACK.getIndex());
             style.setBorderLeft(BorderStyle.THIN);
